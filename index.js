1bb6b736367cb88e2432a86cc7ff5c3c40ed8798
MICHELLEWAIRIMU04 <michelle@michelle>
2024-10-24 08:45:09 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+console.log(upperCaseCustomerName())
+
+function setBestCustomer() {
+    window.bestCustomer = 'not bob';
+}
+console.log(setBestCustomer())
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+console.log(overwriteBestCustomer())
+
+const leastFavoriteCustomer = 'somebody';
+console.log(leastFavoriteCustomer())
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'anybody'; 
+}
+console.log(changeLeastFavoriteCustomer())
